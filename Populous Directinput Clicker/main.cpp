e5b4405ecf84d78000cacafbdd230cb3b38a95e8
Brandantl <admin@brandanlasley.com>
2015-03-30 23:39:28 -0700

    Working

--- Populous Directinput Clicker/main.cpp
+++ Populous Directinput Clicker/main.cpp
@@ -18,21 +18,30 @@ void SendKey(UINT mappedKey)
 
 void main()
 {
-	printf("Start!\n\nPress the space bar! ...\n");
-
-	char Letter = 'W'; 
-
-	SHORT key = VkKeyScanA(VK_RETURN);
-	UINT mappedKey = MapVirtualKey(LOBYTE(key), 0);
-
 	while (true)
 	{
-		if (GetAsyncKeyState(VK_SPACE) < 0)
-		{
-			SendKey(mappedKey);
-			SendKey(MapVirtualKey(VK_SPACE, 0));
-		}
+		INPUT input = { 0 };
+		input.type = INPUT_MOUSE;
+		input.mi.dx = 0;
+		input.mi.dy = 0;
+		input.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN);
+		input.mi.mouseData = 0;
+		input.mi.dwExtraInfo = NULL;
+		input.mi.time = 0;
+		SendInput(1, &input, sizeof(INPUT));
 
-		Sleep(10);
+		Sleep(30);
+
+		INPUT input2 = { 0 };
+		input2.type = INPUT_MOUSE;
+		input2.mi.dx = 0;
+		input2.mi.dy = 0;
+		input2.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTUP);
+		input2.mi.mouseData = 0;
+		input2.mi.dwExtraInfo = NULL;
+		input2.mi.time = 0;
+		SendInput(1, &input2, sizeof(INPUT));
+		Sleep(1000);
 	}
+
 }
